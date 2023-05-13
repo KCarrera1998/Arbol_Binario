4b1cfd12adb6fd87a56db6921ff778364ba32a22
KCarrera1998 <117527849+KCarrera1998@users.noreply.github.com>
2023-05-12 22:46:37 -0600

    Agregar archivos de proyecto.

---
+++ Arbol_Binario/NodoArbol.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbol_Binario
+{
+    internal class NodoArbol
+    {
+        public NodoArbol? SubArbolIzquierdo { get; set; }
+        public NodoArbol? SubArbolDerecho { get; set; }
+        public object? Informacion { get; set; }
+        public NodoArbol()
+        {
+            SubArbolIzquierdo = null;
+            SubArbolDerecho = null;
+            Informacion = null;
+        }
+
+        public NodoArbol(object informacion)
+        {
+            SubArbolIzquierdo = null;
+            SubArbolDerecho = null;
+            Informacion = informacion;
+        }
+
+        public NodoArbol(NodoArbol subArbolIzquierdo, NodoArbol subArbolDerecho, object informacion)
+        {
+            SubArbolIzquierdo = subArbolIzquierdo;
+            SubArbolDerecho = subArbolDerecho;
+            Informacion = informacion;
+        }
+
+        public override string ToString()
+        {
+            return $"/{Informacion}\\";
+        }
+    }
+}
